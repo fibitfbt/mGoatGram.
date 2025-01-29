93b6da52023c5927b8d03b03edf20e3636f941ad
fibitfbt <fibitfbt@gmail.com>
2025-01-29 23:33:54 +0800

    Update Next.js

--- Next.js
+++ Next.js
@@ -1,1 +1,16 @@
 console.log('Next.js Integration Placeholder');
+'use client' // Required for Next.js
+
+import { ReactNode, useEffect } from 'react'
+import { MiniKit } from '@worldcoin/minikit-js'
+
+export default function MiniKitProvider({ children }: { children: ReactNode }) {
+	useEffect(() => {
+		// Passing appId in the install is optional 
+		// but allows you to access it later via `window.MiniKit.appId`
+		MiniKit.install(appId?) 
+	}, [])
+
+	return <>{children}</>
+}
+
