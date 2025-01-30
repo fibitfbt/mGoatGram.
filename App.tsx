c8dbce442f6e610a5c569566345f00495bdbedd0
fibitfbt <fibitfbt@gmail.com>
2025-01-30 23:30:03 +0800

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -3,9 +3,13 @@ import { VerifyBlock } from "./components/Verify";
 
 export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
-      <VerifyBlock />
-      <PayBlock />
+    <main className="grid grid-cols-1 md:grid-cols-2 min-h-screen items-center justify-center p-6 gap-6 bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+      <section className="flex flex-col items-center p-6 bg-white dark:bg-gray-800 shadow-lg rounded-lg">
+        <VerifyBlock />
+      </section>
+      <section className="flex flex-col items-center p-6 bg-white dark:bg-gray-800 shadow-lg rounded-lg">
+        <PayBlock />
+      </section>
     </main>
   );
 }
